f85ed12202be99b2529d030cd354139811345ce5
Ammar-Asim-23 <ammarasim065@gmail.com>
2023-04-29 15:59:34 +0500

    completed restful api

--- server/index.js
+++ server/index.js
@@ -17,35 +17,71 @@ app.post('/todos' , async (req, res) => {
     try{
         const { description } = req.body;
         
-        const newTodo = pool.query("INSERT INTO todo (description) VALUES ($1)"
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *"
         , [description]
         );
 
-        res.json(newTodo);
+        res.json(newTodo.rows[0]);
     }
     catch(err){
-        console.log(err.message)
+        console.error(err.message);
     }
-})
+});
 
 //get all todos
-
+    app.get('/todos' , async (req, res) => {
+        try {
+            const allTodos = await pool.query("SELECT * FROM todo")
+            res.json(allTodos.rows)
+        } catch (error) {
+            console.error(err.message)
+        }
+    });
 
 
 //get a todo 
 
-
+    app.get('/todos/:id' , async (req, res) => {
+        try {
+            const id = req.params.id
+            const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1"
+          , [id])
+          res.json(todo.rows[0]);
+        } catch (error) {
+            console.error(err.message);
+        }
+    });
 
 
 
 //update a todo
 
+    app.put('/todos/:id', async (req, res) => {
+        try {
+            const id = req.params.id;
+            const {description} = req.body;
+            const updatedTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2"
+          , [description, id]);
 
+          res.json('todo was updated');
+
+        } catch (error) {
+            console.error(err.message);
+        }
+    });
 
 
 
 //delete a todo
-
+    app.delete('/todos/:id', async (req, res) => {
+        try {
+            const { id } = req.params;
+            const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+            res.json('Todo was deleted successfully')
+        } catch (error) {
+            console.error(err.message)
+        }
+    })
 
 
 
@@ -53,4 +89,4 @@ app.post('/todos' , async (req, res) => {
 
 app.listen(5000 , () => {
     console.log('server is running')
-})
+});
